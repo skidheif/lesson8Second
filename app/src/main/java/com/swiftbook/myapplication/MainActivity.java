09d9d62da1f8dbee809822d7107f018f469e015b
Charmandik <rbikbulatov@list.ru>
2021-06-28 23:10:23 +0500

    Сверстал экран списка заказов

--- app/src/main/java/com/swiftbook/myapplication/MainActivity.java
+++ app/src/main/java/com/swiftbook/myapplication/MainActivity.java
@@ -1,14 +1,22 @@
 package com.swiftbook.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Switch;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Group;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_orders_list);
+
+
+
+
     }
 }
